7b5c85def2707f6d3bf7b683e50670112372d386
Van Celdran <celdranvan@gmail.com>
2023-01-14 10:46:26 -0800

    added list menu

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
-import "../styles/List.css"
+import React, { useState } from "react";
+import "../styles/List.css";
 const List = () => {
-    return (
-        <div className='List'>
-            <h1>In List</h1>
-        </div>
-    );
-}
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const postItem = { title: title, price: price, image: image }
+    fetch("http://localhost:3000/items", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        
+        },
+        body: JSON.stringify(postItem)
+    })
+  }
+  return (
+    <div className="List">
+      <h1>List an Item</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Title:
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Price:
+          <input
+            type="number"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Image:
+          <input
+            type="text"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          <button>Submit</button>
+        </label>
+      </form>
+    </div>
+  );
+};
 
 export default List;
