6dc28f03ac8178ceaeaec31c96bb9de9fbbf5297
Van Celdran <celdranvan@gmail.com>
2023-01-14 11:04:44 -0800

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import "../styles/Footer.css"
 const Footer = () => {
     return (
         <div className='Footer'>
-            
+            <h4>Mock Copyright Ⓒ 2022 MarketPlace</h4>
         </div>
     );
 }
